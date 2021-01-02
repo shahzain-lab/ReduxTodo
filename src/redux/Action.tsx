b091af98d37ab9ed718c17ffdd52499c069cc4b7
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-02 14:59:04 -0800

    Action types for typescript

--- src/redux/Action.tsx
+++ src/redux/Action.tsx
@@ -4,6 +4,7 @@ import {ActionTypes} from '../interface/types';
 
 const DELETE_TODO = "DELETE_TODO";
 const ADD_TODO = "ADD_TODO"
+const UPDATE_TODO = "UPDATE_TODO"
 
 //add todoes
 export const addTodo =(todo:ActionTypes)=> {
@@ -15,9 +16,16 @@ export const addTodo =(todo:ActionTypes)=> {
 
 //delete todoes
 
-export function deleteTodo(todo:ActionTypes){
+export function deleteTodo(todoId:ActionTypes){
     return{
         type: DELETE_TODO,
         payload: todoId
+    }
+}
+///updated todo
+export function updateTodo(todo:ActionTypes){
+    return{
+        type: UPDATE_TODO,
+        payload: todo,
     }
 }
